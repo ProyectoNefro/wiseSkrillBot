f593850cfa7251d525c8a288968a68243f484191
Imanol <zaapii@gmail.com>
2023-06-27 20:00:30 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+
+    Vite + Vue 3 + Tailwind + DaisyUI + Firebase scaffold
+  </h1>
+
+</template>
+
+<style scoped>
+</style>
